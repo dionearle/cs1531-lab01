a526c33f6694338fcdc9156d2139432c05b5099f
Dion Earle <dion.earle@gmail.com>
2018-07-26 12:49:17 +1000

    Completed strings file

--- strings.py
+++ strings.py
@@ -1,2 +1,7 @@
 strings = ['This', 'list', 'is', 'now', 'all', 'together']
-sentence = ''
+sentence = strings[0]
+for i in strings[1:]:
+    sentence += ' ' + i
+    
+print(sentence)
+print(' '.join(strings))
